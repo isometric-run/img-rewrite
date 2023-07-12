23d520df63c29b3383d41991fa9fcec257a1ce5d
isometric-run <139071998+isometric-run@users.noreply.github.com>
2023-07-12 08:29:10 +0200

    Add clear command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,5 +6,14 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, count: int):
+        embed = discord.Embed(title="",
+                              description=f"Purged `{count}` messages from {ctx.channel}",
+                              color=coloring.GRAY)
+        await ctx.channel.purge(limit=count)
+        await ctx.send(embed=embed)
+
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
