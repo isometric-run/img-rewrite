fb3997053782f391b48a58b6ee94eb0e3aff6e8b
isometric-run <139071998+isometric-run@users.noreply.github.com>
2023-07-12 08:37:33 +0200

    added slowmode and ban cmds

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -15,5 +15,33 @@ class Moderation(commands.Cog):
         await ctx.channel.purge(limit=count)
         await ctx.send(embed=embed)
 
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason = None):
+        if reason is None:
+            reason = "There is no provided reason"
+
+        embed = discord.Embed(title="",
+                              description=f"Kicked {member.mention} for `{reason}`",
+                              color=coloring.GRAY)
+        dmembed = discord.Embed(title="",
+                                description=f"You got kicked from **{ctx.guild.name}** for **{reason}**",
+                                color=coloring.GRAY)
+        
+        await ctx.send(embed=embed)
+        await member.send(embed=dmembed)
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def slowmode(self, ctx, seconds: int):
+        
+        
+        embed = discord.Embed(title="",
+                            description=f"""> **Slowmode set to** `{seconds}` **by {ctx.author.mention}**""",
+                            color=coloring.GRAY)
+        embed.set_author(icon_url=self.bot.user.avatar, name=self.bot.user.name)
+        await ctx.channel.edit(slowmode_delay=seconds)
+        await ctx.reply(embed=embed)
+
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
