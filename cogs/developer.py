9e87db9a74eb7f47268c8ea06dd3456a69ef2c11
isometric-run <139071998+isometric-run@users.noreply.github.com>
2023-07-12 08:04:29 +0200

    Updated Ping Command

--- cogs/developer.py
+++ cogs/developer.py
@@ -7,6 +7,12 @@ async def type(ctx):
     async with ctx.typing():
             await asyncio.sleep(0.5)
 
+
+class MyView(discord.ui.View):
+    @discord.ui.button(label="IMG", style=discord.ButtonStyle.gray, disabled=True)
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message("Woah how did you click this button") 
+
 class Developer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
