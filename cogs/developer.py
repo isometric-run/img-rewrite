e29bd08638f264ae36d1ca5a5959315a87aa7edc
isometric-run <139071998+isometric-run@users.noreply.github.com>
2023-07-12 08:51:26 +0200

    addded commands to utiity

--- cogs/developer.py
+++ cogs/developer.py
@@ -26,6 +26,23 @@ class Developer(commands.Cog):
         await type(ctx)
         embed = discord.Embed(title='Pong!', description=f'Pong! {round(self.bot.latency * 1000)}ms', color=coloring.GRAY)
         await ctx.send(embed=embed, view=IMGBRANDING())
+
+    @commands.command()
+    @commands.is_owner()
+    async def guilds(self, ctx):
+        guild_info = []
+        for guild in self.bot.guilds:
+            bot_member = guild.get_member(self.bot.user.id)
+            join_date = bot_member.joined_at.strftime('%m/%d/%Y') if bot_member else 'N/A'
+            bot_role = discord.utils.get(guild.roles, name='Your Bot Role Name')  # replace with your bot role name
+            
+            guild_info.append(f"{guild.name:<25} {guild.id:<20} {join_date:<20} {bot_role.name if bot_role else 'N/A'}")
+        
+        file_content = '\n'.join(guild_info)
+        with open('tmp/guilds_info.txt', 'w', encoding='utf-8') as file:
+            file.write(file_content)
+        
+        await ctx.send(file=discord.File('tmp/guilds_info.txt'))
                               
 async def setup(bot):
     await bot.add_cog(Developer(bot))
---
+++ cogs/utility.py
@@ -0,0 +1,73 @@
+import discord
+from discord.ext import commands
+from themes import coloring
+import requests
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(aliases=['av'])
+    async def avatar(self, ctx, user: discord.Member = None): # type: ignore
+        if user == None:
+            user = ctx.author
+
+        response = requests.get(user.avatar) # type: ignore
+        with open("tmp/avatar.jpg", "wb") as file:
+            file.write(response.content)
+
+        embed = discord.Embed(title=f"{user.name}`s avatar",
+                              color=coloring.GRAY)
+        embed.set_image(url=user.avatar)
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def banner(self, ctx, user: discord.Member = None):
+        user = user or ctx.author
+        userid = user.id
+        fetchuserid = await self.bot.fetch_user(userid)
+
+        response = requests.get(fetchuserid.banner) # type: ignore
+        with open("tmp/banner.jpg", "wb") as file:
+            file.write(response.content)
+
+        embed = discord.Embed(title="",
+                              description="",
+                              color=coloring.GRAY)
+        embed.set_author(icon_url=user.avatar, 
+                         name=f"{user.name}`s banner")
+        embed.set_image(url=fetchuserid.banner)
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def servericon(self, ctx, server: discord.Guild = None):
+        server = server or ctx.guild
+
+        response = requests.get(server.icon) # type: ignore
+        with open("tmp/servericon.jpg", "wb") as file:
+            file.write(response.content)
+
+        embed = discord.Embed(title=f"{server}`s icon",
+                              description=f"",
+                              color=coloring.GRAY)
+        embed.set_image(url=server.icon)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def serverbanner(self, ctx, server: discord.Guild = None):
+        server = server or ctx.guild
+
+        response = requests.get(server.banner) # type: ignore
+        with open("tmp/serverbanner.jpg", "wb") as file:
+            file.write(response.content)
+
+        embed = discord.Embed(title=f"{server}`s banner",
+                              description=f"",
+                              color=coloring.GRAY)
+        embed.set_image(url=server.banner)
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Utility(bot))
